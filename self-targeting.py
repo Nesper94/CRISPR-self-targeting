ef87ed9a1fe03ec486cfc6d3079753d3527cb433
Juan Camilo Arboleda Rivera <juan.arboleda2@udea.edu.co>
2022-11-10 19:55:48 -0500

    Add try-except clauses to deal with lacking information

--- self-targeting.py
+++ self-targeting.py
@@ -12,16 +12,27 @@ protein_ids = []
 
 # Get feature table of protospacer region
 for i, start, end in zip(df['Refseq ID'], df['Proto-spacer Start'], df['Proto-spacer End']):
-    result = Entrez.efetch(db='nuccore', id=i, seq_start=start, seq_stop=end, rettype='ft')
-    feature_table = result.read().split('\t')
+    try:
+        result = Entrez.efetch(db='nuccore', id=i, seq_start=start, seq_stop=end, rettype='ft')
+        feature_table = result.read().split('\t')
+    except:
+        products.append('No info')
+        protein_ids.append('No info')
+        continue
 
     # Get product name
-    product_idx = feature_table.index('product')
-    products.append(feature_table[product_idx + 1].strip())
+    try:
+        product_idx = feature_table.index('product')
+        products.append(feature_table[product_idx + 1].strip())
+    except:
+        products.append('No protein')
 
     # Get protein ID
-    protein_id_idx = feature_table.index('protein_id')
-    protein_ids.append(feature_table[protein_id_idx + 1].replace('ref|','').strip('\n|'))
+    try:
+        protein_id_idx = feature_table.index('protein_id')
+        protein_ids.append(feature_table[protein_id_idx + 1].replace('ref|','').strip('\n|'))
+    except(ValueError):
+        protein_ids.append('No protein')
 
 # Modify original dataframe
 df['Product'] = products
