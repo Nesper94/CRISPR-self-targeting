f8e7ff0cbdca22bdce66993f6644db02b9ba96ab
Juan Camilo Arboleda Rivera <juan.arboleda2@udea.edu.co>
2022-11-24 19:13:10 -0500

    Get additional taxonomic information

--- self-targeting.py
+++ self-targeting.py
@@ -116,9 +116,6 @@ for i in df['Refseq ID']:
 
 df['TaxID'] = TaxIDs
 
-# Save table to file
-df.to_csv('self-target-proteins.tsv', index=False, sep='\t')
-
 # Get species and genus
 species_l = []
 genus_l = []
@@ -132,3 +129,62 @@ for i in df['Organism']:
 
 df['Species'] = species_l
 df['Genus'] = genus_l
+
+# Get additional taxonomic information
+superkingdom_l = []
+phylum_l = []
+class_l = []
+order_l = []
+family_l = []
+
+for tax_id in df['TaxID']:
+    try:
+        handle = Entrez.efetch(db='taxonomy', id=tax_id)
+        result = Entrez.read(handle)
+        handle.close()
+    except Exception as e:
+        err = 'Error: ' + str(e)
+        print(err)
+        superkingdom_l.append(err)
+        phylum_l.append(err)
+        class_l.append(err)
+        order_l.append(err)
+        family_l.append(err)
+        continue
+
+    superkingdom_ = 'Error: No info'
+    phylum_ = 'Error: No info'
+    class_ = 'Error: No info'
+    order_ = 'Error: No info'
+    family_ = 'Error: No info'
+
+    for i in result[0]['LineageEx']:
+        if i['Rank'] == 'superkingdom':
+            superkingdom_ = i['ScientificName']
+
+        elif i['Rank'] == 'phylum':
+            phylum_ = i['ScientificName']
+
+        elif i['Rank'] == 'class':
+            class_ = i['ScientificName']
+
+        elif i['Rank'] == 'order':
+            order_ = i['ScientificName']
+
+        elif i['Rank'] == 'family':
+            family_ = i['ScientificName']
+
+    superkingdom_l.append(superkingdom_)
+    phylum_l.append(phylum_)
+    class_l.append(class_)
+    order_l.append(order_)
+    family_l.append(family_)
+
+df['Superkingdom'] = superkingdom_l
+df['Phylum'] = phylum_l
+df['Class'] = class_l
+df['Order'] = order_l
+df['Family'] = family_l
+
+# Save table to file
+df.to_csv('self-target-proteins.tsv', index=False, sep='\t')
